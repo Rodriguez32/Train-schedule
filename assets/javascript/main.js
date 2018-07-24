dc78621749538d3d6e2dd674877cb69261d7661d
Azalea <Rodriguez32.ilda0122@gmail.com>
2018-07-24 13:24:14 -0500

    modified html and js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,8 @@
-// Was able to get Firebase to log approprietly. Was not able to get my information to post on my page. 
-// With updated info on trains and arrivals etc. 
-// Page blinks when I submit. 
-// I see my calls work when I console.log with stars. 
+// I got it to work!!!!!!!///////
+// It was not logging onto my page. 
 
 
-
-$(document).ready(function(){
+$(document).ready(function () {
     console.log("Hello World");
 
     // Firebase
@@ -16,102 +13,90 @@ $(document).ready(function(){
         projectId: "train-schedule-94ae9",
         storageBucket: "train-schedule-94ae9.appspot.com",
         messagingSenderId: "316977571972"
-      };
-      
-      firebase.initializeApp(config);
-      console.log(firebase);
+    };
 
-      var dataRef = firebase.database();
+    firebase.initializeApp(config);
+    console.log(firebase);
 
-      // Initial valus
-      var trainName = "";
-      var destination = "";
-      var trainTime = "";
-      var frequency = "";
+    var database = firebase.database();
+
+    $("#button-input").on("click", function (event) {
+        event.preventDefault();
+
+        $(".empty-form").hide();
+        var name = $("#train-name").val().trim();
+        var destination = $("#train-destination").val().trim();
+        var trainTime = $("#train-time").val().trim();
+        var frequency = $("#time-freq").val().trim();
+
+        if (name != "" && destination != "" && trainTime != "" && frequency != "") {
 
 
-      // code for storing and retrieving the most recent user
-      $("#button-input").on("click", function(event){
-          event.preventDefault();
+            database.ref().push({
+                name: name,
+                destination: destination,
+                trainTime: trainTime,
+                frequency: frequency,
+                dateAdded: firebase.database.ServerValue.TIMESTAMP
+            });
 
-          $("#empty-form").hide();
-          trainName = $("#train-name").val().trim();
-          destination = $("#train-destination").val().trim();
-          trainTime = $("#train-time").val().trim();
-          frequency = $("#time-freq").val().trim();
-
-    
-          if (trainName !="" && destination !="" && trainTime !="" && frequency !="") {
-
-          // code for push
-    dataRef.ref().push({
-
-        trainName: trainName,
-        destination: destination,
-        trainTime: trainTime,
-        frequency: frequency,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-        
-    });
-    $("#train-form")[0].reset();
+            $("#train-form")[0].reset();
         } else {
             $("#empty-form").show();
+        }
+    });
 
+    database.ref().on("child_added", function (childSnapshot) {
 
-          }
-      });
+        var childName = childSnapshot.val().name;
+        var childDestination = childSnapshot.val().destination;
+        var childTrainTime = childSnapshot.val().trainTime;
+        var childFrequency = childSnapshot.val().frequency;
 
-    // firebase watcher + initial loader
-    dataRef.ref().on("child_added", function(childSnapshot){
-        console.log("************************************************")
-    // log what is coming from snapshot    
-        console.log(childSnapshot.val().trainName);
-        console.log(childSnapshot.val().destination);
-        console.log(childSnapshot.val().trainTime);
-        console.log(childSnapshot.val().frequency);
-        
+        console.log("firebase" + childName);
+        console.log("firebase" + childDestination);
+        console.log("firebase" + childTrainTime);
+        console.log("firebase" + childFrequency);
 
-        var barTest = childSnapshot.val().trainName;
-     
         var newRow = $("<tr>");
         var newName = $("<td>").attr("data-name", childName).text(childName);
         var newDestinaton = $("<td>").attr("data-name", childDestination).text(childDestination);
         var newRate = $("<td>").attr("data-name", childFrequency).text(childFrequency);
-    
+
         // moment.js
-// convert start time
-    var dateFormat = "HH:mm a";
-    var convertedTime = moment(childTrainTime, dateFormat).subract(1, "years");
-    console.log(convertedTime);
-// logging current time
-    var currentTime = moment();
-    console.log("Current Time: " + moment(currentTime).format("hh:mm a"));
 
-// difference in time
-    var timeDiff = moment().diff(moment(convertedTime), "minutes");
-    console.log("Difference in time: " + timeDiff);
-    
-// check remainder of time diff and the frequency
-    var timeRemain = timeDiff % childFrequency;
-    console.log(timeRemain);
+        // convert start time
+        var dateFormat = "HH:mm a";
+        var convertedTime = moment(childTrainTime, dateFormat).subtract(1, "years");
+        console.log(convertedTime);
+        // logging current time
+        var currentTime = moment();
+        
 
-//subtract the remainder from frequency to find the time for next train
-    var timeNextTrain = childFrequency - timeRemain;
-    console.log("Next Train: " + timeNextTrain);
+        // difference in time
+        var timeDiff = moment().diff(moment(convertedTime), "minutes");
+        console.log("Difference in time: " + timeDiff);
 
-// Next Train
-    var nextTrain = moment().add(timeNextTrain, "minutes");
-    console.log("Arrive Time: " + moment(nextTrain).format("hh:mm a"));
-    var nextTrainFormat = moment(nextTrain).format("hh:mm a");
+        // check remainder of time diff and the frequency
+        var timeRemain = timeDiff % childFrequency;
+        
+        //subtract the remainder from frequency to find the time for next train
+        var timeNextTrain = childFrequency - timeRemain;
+        
+        // Next Train
+        var nextTrain = moment().add(timeNextTrain, "minutes");
+        
+        var nextTrainFormat = moment(nextTrain).format("hh:mm a");
 
-// logging in the next train
-var nextArrival = $("<td>").text(nextTrainFormat);
-var minutesAway = $("<td>").text(timeNextTrain);
+        // logging in the next train
+        var nextArrival = $("<td>").text(nextTrainFormat);
+        var minutesAway = $("<td>").text(timeNextTrain);
 
-newRow.append(newName, newDestinaton, newRate, nextArrival, minutesAway);
-$("#table-body").append(newRow);
+        newRow.append(newName, newDestinaton, newRate, nextArrival, minutesAway);
+        $("#table-data").append(newRow);
 
-
-    });
 
     });
+
+
+});
